b8d309605878aa1370b4acfb1bc7ef50c94b9cb0
Tomeczqueee <46226715+Tomeczqueee@users.noreply.github.com>
2019-05-23 12:09:23 +0000

    1f12f

--- js/index.js
+++ js/index.js
@@ -8,31 +8,54 @@ function scrollFunction(){
         document.getElementById("header").style.padding = "80px 10px";
         document.getElementById("logo").style.fontSize = "35px";
       }
-      console.log("działa");
 }
 
 //https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
-
-const app = {};
+//https://www.hongkiat.com/blog/node-js-server-side-javascript/
+ const app = {};
 //https://www.youtube.com/watch?v=f5OLDvwP-Ug
 //https://developer.spotify.com/console/get-artist/?id=
 //base64 c7236ae5231e4ef992670b2889beb6d7:39989bd86c8a44e9af3f0470cb9e9270
 //https://feel-music.com
-//
+
+
+
 app.tokenOptions = {
     url: 'https://accounts.spotify.com/authorize',
-    client_id: 'client_id=c7236ae5231e4ef992670b2889beb6d7',
+    client_id: 'client_id=c48bf293c8c341908c1283e89a797670',
     headers:{
         'Content-Type':"application/json"
     },
     response_type: 'response_type=token',
-    redirect_uri: `redirect_uri=${encodeURIComponent(`http://127.0.0.1:8080/`)}`,
+    redirect_uri: `redirect_uri=${encodeURIComponent(`http://127.0.0.1:8080/index.html`)}`,
 
     parseToUrl: () => {
         return `${app.tokenOptions.url}?${app.tokenOptions.client_id}&${app.tokenOptions.redirect_uri}&${app.tokenOptions.response_type}`
-        }
-    };
+    }
 };
+console.log();
+
+    if(window.location.href == "http://127.0.0.1:8080/index.html"){
+        (function redirectForToken(){
+            window.location = app.tokenOptions.parseToUrl();
+        })()
+    };
+
+
+
+//
+// fetch("https://accounts.spotify.com/api/token", {
+//     body: "grant_type=client_credentials",
+//     headers: {
+//         Authorization: "Basic YzcyMzZhZTUyMzFlNGVmOTkyNjcwYjI4ODliZWI2ZDc6Mzk5ODliZDg2YzhhNDRlOWFmM2YwNDcwY2I5ZTkyNzA=",
+//         "Content-Type": "application/x-www-form-urlencoded"
+//     },
+//     method: "POST"
+// })
+//     .then(success => console.log(success))
+
+
+
 
 // app.tokenRefresh = () => {
 //     fetch("https://accounts.spotify.com/api/token", {
