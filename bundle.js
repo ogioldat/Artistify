9b7ff697a26fac33cc608e3871280b64799b3f9f
Tomeczqueee <0faighter123@gmail.com>
2019-05-23 23:02:36 +0000

    Working spotify token authorisation

--- bundle.js
+++ bundle.js
@@ -1,4 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const app = {};
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction(){
@@ -11,106 +13,96 @@ function scrollFunction(){
       }
 }
 
-//https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
-//https://www.hongkiat.com/blog/node-js-server-side-javascript/
- const app = {};
-//https://www.youtube.com/watch?v=f5OLDvwP-Ug
-//https://developer.spotify.com/console/get-artist/?id=
+
 //base64 c7236ae5231e4ef992670b2889beb6d7:39989bd86c8a44e9af3f0470cb9e9270
-//https://feel-music.com
-
-
 
 app.tokenOptions = {
-    url: 'https://accounts.spotify.com/authorize',
-    client_id: 'client_id=c48bf293c8c341908c1283e89a797670',
+    url: 'https://accounts.spotify.com/en/authorize',
+    client_id: 'client_id=6021202dc4a242109a7f7629cb971347',
     headers:{
         'Content-Type':"application/json"
     },
     response_type: 'response_type=token',
-    redirect_uri: `redirect_uri=${encodeURIComponent(`http://127.0.0.1:8080/index.html`)}`,
+    redirect_uri: `redirect_uri=${encodeURIComponent(`http://127.0.0.1:8080`)}`,
 
     parseToUrl: () => {
         return `${app.tokenOptions.url}?${app.tokenOptions.client_id}&${app.tokenOptions.redirect_uri}&${app.tokenOptions.response_type}`
     }
 };
-console.log();
 
-    if(window.location.href == "http://127.0.0.1:8080/index.html"){
-        (function redirectForToken(){
+app.urlRegExp = /[0-9a-zA-Z!@#$%^&*./=+_-]access_token[0-9a-zA-Z!@#$%^&*./=+_-]/;
+
+    if(!(app.urlRegExp.test(window.location.href))){
             window.location = app.tokenOptions.parseToUrl();
-        })()
-    };
+            app.tokenCode();
+    }
+
+app.tokenCode = () => {
+    let tokenSearch = window.location.href;
+    let tokenStart = tokenSearch.substr(tokenSearch.indexOf('access_token')+13,tokenSearch.length);
+    return tokenStart.substr(0,tokenStart.indexOf('&'));
+};
+
+app.requestOptions = {
+    method:"GET",
+    headers: new Headers({
+        "Accept":"application/json",
+        "Content-Type":"application/json",
+        "Authorization":`Bearer ${app.tokenCode()}`
+    })
+};
 
 
+app.request = nameToSend =>{
+    fetch(`https://api.spotify.com/v1/search?q=${nameToSend}&type=track&limit=5`,app.requestOptions)
+        .then(response => response.json()
+            .then(data => {
+                console.log(data);
+            })
+            .then(data =>({
+                data: data,
+                status: response.status
+            }))
+            .then(res => console.log(res.data))
+        )
 
-//
-// fetch("https://accounts.spotify.com/api/token", {
-//     body: "grant_type=client_credentials",
-//     headers: {
-//         Authorization: "Basic YzcyMzZhZTUyMzFlNGVmOTkyNjcwYjI4ODliZWI2ZDc6Mzk5ODliZDg2YzhhNDRlOWFmM2YwNDcwY2I5ZTkyNzA=",
-//         "Content-Type": "application/x-www-form-urlencoded"
-//     },
-//     method: "POST"
-// })
-//     .then(success => console.log(success))
+        .catch(error => console.error(error));
+};
 
 
+app.spaceToPlus = givenSong => {
+    (givenSong[0] === ' ') ? app.request(givenSong.replace(' ', ''))
+    : app.request(givenSong.replace(' ', '+'));
+};
 
 
-// app.tokenRefresh = () => {
-//     fetch("https://accounts.spotify.com/api/token", {
-//         body: "grant_type=client_credentials",
-//         mode: 'no-corps',
-//         headers: {
-//             Authorization: "Basic YzcyMzZhZTUyMzFlNGVmOTkyNjcwYjI4ODliZWI2ZDc6Mzk5ODliZDg2YzhhNDRlOWFmM2YwNDcwY2I5ZTkyNzA=",
-//             "Content-Type": "application/x-www-form-urlencoded"
-//         },
-//         method: "POST"
-//     })
-//         .then(result => console.log(result));
-// };
-//
-// app.tokenRefresh();
+app.masterCallback = songName => {app.spaceToPlus(songName)};
+app.masterCallback('Icon');
 
-//
-// app.requestOptions = {
-//     method:"GET",
-//     headers: new Headers({
-//         "Accept":"application/json",
-//         "Content-Type":"application/json",
-//         "Authorization":`Bearer ${app.token}`
-//     })
-// };
-//
-//
-// app.request = nameToSend =>{
-//     fetch(`https://api.spotify.com/v1/search?q=${nameToSend}&type=track&limit=5`,app.requestOptions)
-//         .then(response => response.json()
-//             .then(data => {
-//                 if(response.status===401) app.tokenRefresh(app.token);
-//             })
-//             .then(data =>({
-//                 data: data,
-//                 status: response.status
-//             }))
-//             .then(res => console.log(res.data))
-//         )
-//
-//         .catch(error => console.error(error));
-// };
-//
-//
-// app.spaceToPlus = givenSong => {
-//     (givenSong[0] === ' ') ? app.request(givenSong.replace(' ', ''))
-//     : app.request(givenSong.replace(' ', '+'));
-// };
-//
-//
-// app.masterCallback = songName => {app.spaceToPlus(songName)};
-// app.masterCallback('Icon');
-//
-//
-//
+app.geniusOptions = {
+    method:'GET',
+    headers: new Headers({
+        'User-Agent': 'CompuServe Classic/1.22',
+        'Accept': 'application/json',
+        'Authorization': 'vHCLxBbQPd1oxjkXzOep8GmrcwbefBLtCo17yp_0oWUgg0sHEUF9mPmFJ3apQFaZ'
+    })
+};
+
+app.geniusTokenOptions = {
+    url: 'https://api.genius.com/oauth/authorize?',
+    client_id: 'client_id=b7e880K0QC9MIO774s35GNiB66aKZejAqghmRTdgmB5NGlUGU58qhotYoebJibB1'+'&',
+    headers:{
+        'Content-Type':"application/json"
+    },
+    redirect_uri: `redirect_uri=${encodeURIComponent(`http://127.0.0.1:8080`)}&`,
+
+    parseToUrl: () => {
+        //https://YOUR_REDIRECT_URI/?code=CODE&state=SOME_STATE_VALUE
+        return `${app.geniusTokenOptions.redirect_uri}?${app.tokenOptions.client_id}&${app.tokenOptions.redirect_uri}&${app.tokenOptions.response_type}`
+    }
+};
+
+fetch('https://api.genius.com/artists/16775', app.geniusOptions)
+    .then(result => console.log(result))
 
 },{}]},{},[1]);
